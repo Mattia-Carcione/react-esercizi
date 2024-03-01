f7df801d4072e97c62bd39e2ca31d30ec734bddb
Mattia <mattia@MacBook-Pro-di-Mattia.local>
2024-03-01 16:29:41 +0100

    init

---
+++ appuntamenti/inizio/src/List.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <ul>
+
+    </ul>
+  );
+};
+
+const Person = () => {
+  return (
+    <article>
+
+    </article>
+  );
+};
+
+export default List;
