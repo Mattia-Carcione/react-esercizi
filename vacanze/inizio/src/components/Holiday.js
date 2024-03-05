2e002d1d1f051ce7ece1231febb6b20d19211a71
Mattia <mattia@MBP-di-Mattia.station>
2024-03-05 18:53:48 +0100

    add soluzione esercizio vacanze

--- vacanze/inizio/src/components/Holiday.js
+++ vacanze/inizio/src/components/Holiday.js
@@ -4,7 +4,55 @@ import SingleHoliday from "./SingleHoliday";
 const url = "https://react--course-api.herokuapp.com/api/v1/data/vacanze";
 
 const Holiday = () => {
-  return <div>Vacanza</div>;
+  const [data, setData] = useState([]);
+  const [selected, setSelected] = useState(0);
+
+  // con un try catch fetcho i dati dall'url e li assegno a data
+  const getData = async () => {
+    try {
+      const response = await axios.get(url);
+      setData(response.data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // con useEffect al render del componente parte la fetch solo una volta 
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // creo una funzione per aumentare selected
+  const next = () => {
+    setSelected((preValue) => {
+      if (preValue + 1 === data.data.length) {
+        return 0;
+      }
+      return preValue + 1;
+    });
+  }
+
+  // creo una funzione per diminuire selected
+  const previous = () => {
+    setSelected((nextValue) => {
+      if (nextValue === 0) {
+        return (data.data.length - 1);
+      } 
+      return nextValue - 1;
+    })
+  }
+
+  // condiziono il return in base alla promise se viene risolta o meno
+  if (data.success) {
+    return (
+      <>
+        {/* con il ternary operator reinderizzo il componente se sono presenti dati */}
+        {data.data.length > 0 ? <SingleHoliday {...data.data[selected]} next={next} previous={previous}/> : <h4>No vacanze</h4>}
+      </>
+    )
+  }
+  return <h4>...loading</h4>
 };
 
 export default Holiday;
--- vacanze/inizio/src/components/Title.js
+++ vacanze/inizio/src/components/Title.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-const Title = () => {
-  return <div>Titolo</div>;
+const Title = ({text}) => {
+  return (
+    <div>
+      <h4>{text || 'nuovo titolo'}</h4>
+    </div>);
 };
 
 export default Title;
