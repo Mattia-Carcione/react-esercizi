32e6d0e0c777767fdfa572ae3327af63f0cf9cf6
Mattia <mattia@MBP-di-Mattia.station>
2024-03-10 11:18:40 +0100

    add progetto slider

---
+++ slider/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <section className="container slider">
+        Slider
+      </section>
+    </div>
+  );
+}
+
+export default App;
